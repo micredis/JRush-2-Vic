046061fbbb4aa2cd2c74b71bdc173fc94b2e16c6
micredis <micredis@gmail.com>
2018-04-06 00:25:33 +0300

    Add tasks 0122, 0123, 0127, 0128, 0135, 0138

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        //напишите тут ваш код
+        System.out.println("Варкалось, хливкие шорьки");
+        System.out.println("Пырялись по мове");
+        System.out.println("И хрюкотали зелюки,");
+        System.out.println("Как мюмзики в мове...");
+    }
+}
