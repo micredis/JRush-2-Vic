7f3c13fa283833946f8e76beff3d9e3c8c9ad67e
micredis <micredis@gmail.com>
2018-04-06 13:16:59 +0300

    Add task0201, task0202

---
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0202/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task02.task0202;
+
+/* 
+Откуда берутся Person?
+*/
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Person person = new Person();
+    }
+
+    private static class Person {
+        //напишите тут ваш код
+        private String name;
+        private int age;
+        private int weight;
+        private int money;
+    }
+}
