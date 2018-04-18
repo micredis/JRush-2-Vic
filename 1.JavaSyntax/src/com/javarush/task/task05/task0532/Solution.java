b5edea8591339f1a20ec7ef35faa568b22088d0c
micredis <micredis@gmail.com>
2018-04-18 12:13:08 +0300

    Update task 0532 to pass the validator (the first number must not be included in the search for the maximum)

--- 1.JavaSyntax/src/com/javarush/task/task05/task0532/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0532/Solution.java
@@ -9,10 +9,10 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int maximum = Integer.parseInt(reader.readLine());
+        int maximum = Integer.MIN_VALUE;
 
         //напишите тут ваш код
-        for (int i = 0; i < maximum; i++) {
+        for (int i = 0, n = Integer.parseInt(reader.readLine()); i < n; i++) {
             int num = Integer.parseInt(reader.readLine());
             if (maximum < num) {
                 maximum = num;
@@ -20,5 +20,6 @@ public class Solution {
         }
 
         System.out.println(maximum);
+        reader.close();
     }
 }
