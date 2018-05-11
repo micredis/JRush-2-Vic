124d0423be5e13a844119506b5dd05218f23ab17
micredis <micredis@gmail.com>
2018-05-11 15:10:05 +0300

    Add solutions for the tasks 1201-1210

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(41);
+        print(Integer.valueOf(95));
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int i) {
+        System.out.println(i);
+    }
+
+    public static void print(Integer i) {
+        System.out.println(i);
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    static void print() {
+        System.out.println();
+    }
+
+    static void print(int i) {
+        System.out.println(i);
+    }
+
+    static void print(long l) {
+        System.out.println(l);
+    }
+
+    static void print(double d) {
+        System.out.println(d);
+    }
+
+    static void print(int i, long l, double d) {
+        System.out.println(i + " " + l + " " + d);
+    }
+}
