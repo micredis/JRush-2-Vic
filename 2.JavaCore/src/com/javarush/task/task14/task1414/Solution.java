abfe7c16ecae7963a87f6d73c6d749e22efd679f
micredis <micredis@gmail.com>
2018-05-24 10:23:32 +0300

    Add solutions for the tasks 1412-1415

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
@@ -0,0 +1,76 @@
+package com.javarush.task.task14.task1414;
+
+/* 
+MovieFactory
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Solution {
+    private static final Set<String> keys;
+    static {
+        Set<String> set = new HashSet<>(Arrays.asList("cartoon", "thriller", "soapOpera"));
+        keys = Collections.unmodifiableSet(set);
+    }
+
+    public static void main(String[] args) throws Exception {
+        //ввести с консоли несколько ключей (строк), пункт 7
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<String> movies = new ArrayList<>();
+        String key;
+        do {
+            key = reader.readLine();
+            movies.add(key);
+        } while (keys.contains(key));
+
+        /*
+8 Создать переменную movie класса Movie и для каждой введенной строки(ключа):
+8.1 получить объект используя MovieFactory.getMovie и присвоить его переменной movie
+8.2 вывести на экран movie.getClass().getSimpleName()
+        */
+
+        Movie movie = null;
+        for (String movieType : movies) {
+            movie = MovieFactory.getMovie(movieType);
+            if (movie != null)
+                System.out.println(movie.getClass().getSimpleName());
+        }
+    }
+
+    static class MovieFactory {
+
+        static Movie getMovie(String key) {
+            Movie movie = null;
+
+            //создание объекта SoapOpera (мыльная опера) для ключа "soapOpera"
+            if ("soapOpera".equals(key)) {
+                movie = new SoapOpera();
+            } else if ("cartoon".equals(key)) {
+                movie = new Cartoon();
+            } else if ("thriller".equals(key)) {
+                movie = new Thriller();
+            }
+
+            //напишите тут ваш код, пункты 5,6
+
+            return movie;
+        }
+    }
+
+    static abstract class Movie {
+    }
+
+    static class SoapOpera extends Movie {
+    }
+
+    //Напишите тут ваши классы, пункт 3
+    static class Cartoon extends Movie {
+
+    }
+
+    static class Thriller extends Movie {
+
+    }
+}
