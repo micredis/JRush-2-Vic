949be027a4430bd2a32c8da85865aeb36cb33ce7
micredis <micredis@gmail.com>
2018-05-29 13:21:23 +0300

    Add a solutions for the task 1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    public SubSolution() {
+    }
+
+    protected SubSolution(byte b) {
+        super(b);
+    }
+
+    SubSolution(short s) {
+        super(s);
+    }
+}
