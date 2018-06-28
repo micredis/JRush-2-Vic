ee613cdc2315273dbb5e1a62f07755a4a072e13c
micredis <micredis@gmail.com>
2018-06-28 18:03:47 +0300

    Modify the solution for the task 1827 to comply with the controversial demands of the validator. Preserve my previous solution for this task as an archive in the task18 package

--- 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -8,81 +8,43 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Solution {
-    private String fileName;
-    private int id;
-    private String productName = "";
-    private String price;
-    private String quantity;
-
     public static void main(String[] args) throws Exception {
-        if (args.length == 0) return;
-        if (!"-c".equals(args[0])) return;
-        new Solution().go(args);
+        if (args.length != 0 && "-c".equals(args[0])) {
+            new Solution().go(args);
+        }
     }
 
     private void go(String[] args) {
-        this.fileName = readFileName();
-        initFields(args);
-        String record = String.format(
-                "%-8d%-30.30s%-8.8s%-4.4s",
-                this.id,
-                this.productName,
-                this.price,
-                this.quantity
-        );
-//        if (this.id == 1) {
-//            record = String.format(
-//                    "%-8d%-30.30s%-8.8s%-4.4s",
-//                    this.id,
-//                    this.productName,
-//                    this.price,
-//                    this.quantity
-//            );
-//        } else {
-//            record = String.format(
-//                    "%n%-8d%-30.30s%-8.8s%-4.4s",
-//                    this.id,
-//                    this.productName,
-//                    this.price,
-//                    this.quantity
-//            );
-//        }
-        addRecord(fileName, record);
+        String fileName = readFileName();
+        createRecord(args, fileName);
+    }
+
+    private void createRecord(String[] args, String fileName) {
+        int id = getMaxID(fileName) + 1;
+        try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(fileName, true))) {
+            String record = String.format("%-8d%-30.30s%-8.8s%-4.4s", id, args[1], args[2], args[3]);
+            fileWriter.newLine();
+            fileWriter.write(record);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private String readFileName() {
-        String fileName = "";
+        String fileName;
         Scanner scanner = new Scanner(System.in);
-        if (scanner.hasNext())
-            fileName = scanner.nextLine();
+        fileName = scanner.nextLine();
         scanner.close();
         return fileName;
     }
 
-    private void initFields(String[] args) {
-        // -c Toolbox and a horse with a quite big nose 123.9 5
-        this.id = getLastID(fileName) + 1;
-        int i;
-        for (i = 1;
-             i < args.length - 2 && this.productName.length() < 30;
-             i++) {
-            this.productName += args[i];
-            this.productName += " ";
-        }
-        this.productName = this.productName.substring(0, 30);
-        this.price = args[args.length - 2];
-        this.quantity = args[args.length - 1];
-    }
-
-    private int getLastID(String fileName) {
+    private int getMaxID(String fileName) {
         int maxID = 0;
-        try (BufferedReader fileReader =
-                     new BufferedReader(
-                             new InputStreamReader(
-                                     new FileInputStream(fileName)))) {
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName))) {
+            int id;
             while (fileReader.ready()) {
                 String line = fileReader.readLine();
-                int id = Integer.parseInt(line.substring(0, 8).trim());
+                id = Integer.parseInt(line.substring(0, 8).trim());
                 if (id > maxID)
                     maxID = id;
             }
@@ -90,18 +52,5 @@ public class Solution {
             e.printStackTrace();
         }
         return maxID;
-    }
-
-    private void addRecord(String fileName, String record) {
-        try (FileOutputStream fos = new FileOutputStream(fileName, true)) {
-            if (this.id == 1) {
-                fos.write(record.getBytes());
-            } else {
-                fos.write("\n".getBytes());
-                fos.write(record.getBytes());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
